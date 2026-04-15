661b40228adf77baab621434353699462538ee73
Enmanuel <enmanuel@MacBook-Neo-de-Enmanuel.local>
2026-04-15 12:16:22 -0400

    Edit vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/web_project_around_react/",
   server: {
     port: 3000,
     watch: {
       usePolling: true,
     },
   },
-})
+});
